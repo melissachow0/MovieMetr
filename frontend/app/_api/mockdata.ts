9abb545ab941568095a04143e49611bfb8e18a52
melissachow0 <melychow@icloud.com>
2024-11-14 22:25:01 -0800

    added description

--- frontend/app/_api/mockdata.ts
+++ frontend/app/_api/mockdata.ts
@@ -5,35 +5,57 @@ export const mockplayingData = [
     image: "despicableme4.jpg",
     title: "Despicable Me 4",
     backdrop_path: "despicableme4bd.jpg",
-    summary: "Summary of Now Playing Movie 1",
+    summary:
+      "Things just got a little more despicable. Gru and Lucy and their girls—Margo, Edith and Agnes—welcome a new member to the Gru family, Gru Jr., who is intent on tormenting his dad. Gru also faces a new nemesis in Maxime Le Mal and his femme fatale girlfriend Valentina, forcing the family to go on the run.",
   },
   {
     id: 2,
     image: "deadpoolandwolverine.jpg",
     backdrop_path: "deadpoolandwolverinebd.jpg",
     title: "Deadpool and Wolverine",
-    summary: "Summary of Now Playing Movie 1",
+    summary:
+      "Come together. A listless Wade Wilson toils away in civilian life with his days as the morally flexible mercenary, Deadpool, behind him. But when his homeworld faces an existential threat, Wade must reluctantly suit-up again with an even more reluctant Wolverine.",
   },
   {
     id: 3,
     image: "gladiator2.jpg",
     backdrop_path: "gladiator2bd.jpg",
     title: "Gladiator II",
-    summary: "Summary of Now Playing Movie 1",
+    summary:
+      "Years after witnessing the death of the revered hero Maximus at the hands of his uncle, Lucius is forced to enter the Colosseum after his home is conquered by the tyrannical Emperors who now lead Rome with an iron fist. With rage in his heart and the future of the Empire at stake, Lucius must look to his past to find strength and honor to return the glory of Rome to its people.",
   },
   {
     id: 4,
     image: "thewildrobot.jpg",
     backdrop_path: "thewildrobotbd.jpg",
     title: "The Wild Robot",
-    summary: "Summary of Now Playing Movie 1",
+    summary:
+      "Discover your true nature. After a shipwreck, an intelligent robot called Roz is stranded on an uninhabited island. To survive the harsh environment, Roz bonds with the island's animals and cares for an orphaned baby goose.",
   },
   {
     id: 5,
     image: "transformerone.jpg",
     backdrop_path: "transformersbd.jpg",
     title: "Transformer One",
-    summary: "Summary of Now Playing Movie 1",
+    summary:
+      "The untold origin story of Optimus Prime and Megatron, better known as sworn enemies, but once were friends bonded like brothers who changed the fate of Cybertron forever.",
+  },
+
+  {
+    id: 6,
+    image: "venom.jpg",
+    backdrop_path: "venombd.jpg",
+    title: "Venom",
+    summary:
+      "'Til death do they part.Eddie and Venom are on the run. Hunted by both of their worlds and with the net closing in, the duo are forced into a devastating decision that will bring the curtains down on Venom and Eddie's last dance.",
+  },
+  {
+    id: 7,
+    image: "thejoker.jpg",
+    backdrop_path: "jokerbd.jpg",
+    title: "Joker: Folie à Deux",
+    summary:
+      "The world is a stage. While struggling with his dual identity, Arthur Fleck not only stumbles upon true love, but also finds the music that's always been inside him.",
   },
   // Add more "Now Playing" movies here
 ];
